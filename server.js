00dbbfe444cbe9eefe13d77ead69ebf0165ea14e
Catherine Emond <catherine.dnome@gmail.com>
2020-08-04 11:56:07 -0400

    modified to add rooms and use Redis for pubsub

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-var app = require("express")();
-var http = require("http").createServer(app);
-var io = require("socket.io")(http);
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const socketioRedis = require("socket.io-redis");
+const config = require("./config.js");
+const process = require("process");
+const port = process.argv[2];
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static("static"));
+
+io.adapter(socketioRedis({ host: config.redis_host, port: config.redis_port }));
 
 io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    const text = `A user says: ${msg}`;
-    io.emit("chat message", text);
+  socket.on("room-join", (room) => {
+    socket.join(room);
+    socket.emit("event", "Joined room " + room);
+    socket.broadcast.to(room).emit("event", "Someone joined room " + room);
+  });
+
+  socket.on("event", (e) => {
+    socket.broadcast.to(e.room).emit("event", e.name + " says hello!");
   });
 });
 
-http.listen(3000, () => {
-  console.log("server started");
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
